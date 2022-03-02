305917f581d13766acb8d34304c3bac8ca843f2f
Ronoaldo JLP <ronoaldo@gmail.com>
2022-03-02 08:47:22 -0600

    Allow docker to capture logs from container.

--- minetest-wrapper.sh
+++ minetest-wrapper.sh
@@ -2,8 +2,8 @@
 
 # Main loop
 while true ; do
-	echo "-- Separator --" >> /tmp/minetest.stderr
-	minetestserver $@ 2>> /tmp/minetest.stderr
+	echo -e "\n\n-- Separator --\n\n" >> /tmp/minetest.stderr
+	minetestserver $@ 2>&1 | tee -a /tmp/minetest.stderr
 	echo "Minetest server crashed! See error logs at debug.txt and /tmp/minetest.stderr"
 	echo "Restarting in 5s ..."
 	sleep 5
