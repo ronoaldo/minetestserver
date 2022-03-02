d90234853c7b5e7770ed60a3e22e5bc1bce57f42
Ronoaldo JLP <ronoaldo@gmail.com>
2022-03-02 08:48:14 -0600

    Using wrapper from stable-5 branch.

--- minetest-wrapper.sh
+++ minetest-wrapper.sh
@@ -2,8 +2,9 @@
 
 # Main loop
 while true ; do
-	minetestserver $@
-	echo "Minetest server crashed! See error logs at debug.txt"
+	echo -e "\n\n-- Separator --\n\n" >> /tmp/minetest.stderr
+	minetestserver $@ 2>&1 | tee -a /tmp/minetest.stderr
+	echo "Minetest server crashed! See error logs at debug.txt and /tmp/minetest.stderr"
 	echo "Restarting in 5s ..."
 	sleep 5
 done
