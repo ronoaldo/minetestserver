d034b3ae14952b3e02a80a5f7d1db9b8c5525dfb
Ronoaldo JLP <ronoaldo@gmail.com>
2022-07-10 15:39:05 -0300

    Added an option to override stdout/stderr file.
    Resolves #6 by allowing downstream servers
    to pass --quiet and better control debug files

--- minetest-wrapper.sh
+++ minetest-wrapper.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+MINETEST_STDERR_FILE=${MINETEST_STDERR_FILE:-/tmp/minetest.stderr}
+
 # Main loop
 while true ; do
-	echo -e "\n\n-- Separator --\n\n" >> /tmp/minetest.stderr
-	minetestserver $@ 2>&1 | tee -a /tmp/minetest.stderr
-	echo "Minetest server crashed! See error logs at debug.txt and /tmp/minetest.stderr"
-	echo "Restarting in 5s ..."
-	sleep 5
+	echo -e "\n\n-- Separator --\n\n" >> ${MINETEST_STDERR_FILE}
+	minetestserver $@ 2>&1 | tee -a ${MINETEST_STDERR_FILE}
+	echo "Minetest server crashed! See error logs at debug.txt and ${MINETEST_STDERR_FILE}"
+	echo "Restarting in 10s ..."
+	sleep 10
 done
