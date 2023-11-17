aac05b12a19501c908fb02713fe83df2c62a926a
Ronoaldo JLP <ronoaldo@gmail.com>
2023-11-17 00:15:13 -0300

    ci: allow testing to run non-interactive.
    This change makes the quickstart.sh bigger,
    but should not hurt.

--- sample/quickstart.sh
+++ sample/quickstart.sh
@@ -1,24 +1,28 @@
 #!/bin/bash
+# shellcheck disable=2086
 set -x
 set -e
 
+# 0. Setup some environment variables for testing
 if [ -n "$1" ]; then
 	export BUILD_ARGS="--build-arg BASE_IMAGE=$1"
 fi
+if [ -t 1 ] ; then 
+	export IT=-it
+fi
 
 # 1. (Re)build the image
-# shellcheck disable=2086
 docker build -t myserver:latest ${BUILD_ARGS} ./
 
 # 2. Prepare/fix the data dir
 mkdir -p data
-docker run -it --rm \
+docker run ${IT} --rm \
 	-v "$PWD:/server" \
 	-u 0:0 \
 	myserver:latest bash -c 'chown -R minetest /server/data'
 
 # 3. Run the server without the restart loop.
-docker run -it --rm --name minetest_server \
+docker run ${IT} --rm --name minetest_server \
 	-v "$PWD/data:/var/lib/minetest" \
 	-p 30000:30000/udp -p 30000:30000/tcp \
 	-e NO_LOOP=true \
